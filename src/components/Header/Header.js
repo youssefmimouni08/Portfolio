428aa8b14dad8160198611061dfbcc87d074a469
youssefmimouni08 <45660557+youssefmimouni08@users.noreply.github.com>
2022-02-28 14:49:04 +0100

    New Resume

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ const Header = () => (
             color: 'white',
           }}
         >
-          <DiCssdeck size='3rem' /> <Span>Portfolio</Span>
+          <DiCssdeck size='3rem' /> <Span>Portfolio aaaa</Span>
         </a>
       </Link>
     </Div1LOGO>
